7a08479614f87baaf0afe895e7177cd594c35dcc
Daan <daan.heikens@hva.nl>
2019-09-19 15:56:24 +0200

    fixed modules

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
