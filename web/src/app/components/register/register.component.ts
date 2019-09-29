6c2710be2c114fab11181074b4a852836eddbf77
Daan <daan.heikens@hva.nl>
2019-09-29 11:32:46 +0200

    Added route, form and request

--- web/src/app/components/register/register.component.ts
+++ web/src/app/components/register/register.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserService} from '../../services/user.service';
+import {first} from 'rxjs/operators';
+import {HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = '';
 
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService
+  ) {
+
   }
 
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      email: ['', Validators.required, Validators.email]
+    });
+
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/game';
+  }
+
+  get formControls() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    const body = new HttpParams()
+      .set('username', this.formControls.username.value)
+      .set('password', this.formControls.password.value)
+      .set('email', this.formControls.email.value);
+
+    this.loading = true;
+    this.userService.register(body)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        });
+  }
 }
--- web/src/app/services/user.service.ts
+++ web/src/app/services/user.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {User} from '../model/User';
 import {environment} from '../../environments/environment';
+import {map} from 'rxjs/operators';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class UserService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
 
-  getAll() {
-    return this.http.get<User[]>(`${environment.apiUrl}/users`);
+  }
+
+  register(body: HttpParams) {
+    const headers = {
+      'Content-type': 'application/x-www-form-urlencoded'
+    };
+
+    return this.http.post<User>(`${environment.apiUrl}/user/create`, body, {headers})
+      .pipe(map(user => {
+        return user;
+      }));
   }
 }
