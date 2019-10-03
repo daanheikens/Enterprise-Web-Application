db8e2dc52e27b0319f394fec3a24ac67f008b82b
Daan <daan.heikens@hva.nl>
2019-10-03 12:18:18 +0200

    Made storage properties package public only

--- api/src/main/java/com/hva/nl/ewa/services/storage/StorageProperties.java
+++ api/src/main/java/com/hva/nl/ewa/services/storage/StorageProperties.java
@@ -3,7 +3,7 @@ package com.hva.nl.ewa.services.storage;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "storage")
-public class StorageProperties {
+class StorageProperties {
 
     private String location;
 
