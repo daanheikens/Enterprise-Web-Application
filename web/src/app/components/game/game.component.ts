a02c744ff8c270d2a96b4cf15a99b311b3a43e08
Daan <daan.heikens@hva.nl>
2019-10-10 16:49:51 +0200

    Added some comments to work on

--- web/src/app/components/game/game.component.ts
+++ web/src/app/components/game/game.component.ts
@@ -14,18 +14,20 @@ export class GameComponent implements OnInit {
 
   public placeAbleTile: Tile;
 
-  private game: Game;
+  private readonly game: Game;
 
   constructor(private gameService: GameService) {}
 
   ngOnInit() {
+    // For testing purposes we can create a room with just 1 person and play with 1 person
+    // without turns to create the game mechanics
     // Load the game in steps:
     /**
      * Step 1 fetch the game of the user
      * Step 2 Connect to the correct room
      * Step 3 Wait until (enough) players join the game
      * Step 4 Fetch the board of this game when all players are joined
-     * Step 5 start the game
+     * Step 5 start the game (Current rule: Initiator starts first)
      */
   }
 
