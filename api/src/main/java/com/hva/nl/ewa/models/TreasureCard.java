b1177bc99f234ed5b40202907f1aa0c0d8be23cc
griends1 <Steelpan1>
2019-11-17 14:00:15 +0100

    treasurecards toegevoegd restructering tiles.

---
+++ api/src/main/java/com/hva/nl/ewa/models/TreasureCard.java
@@ -0,0 +1,17 @@
+package com.hva.nl.ewa.models;
+
+public class TreasureCard {
+
+    private String imgSource;
+    private int tileDefinitionId;
+
+    public TreasureCard( int tileDefinitionId, String imgSource) {
+        this.tileDefinitionId = tileDefinitionId;
+        this.imgSource = imgSource;
+    }
+
+
+//    private static final TreasureCard[] TREASURE_CARDS = {
+//        new TreasureCard(1, TreasureStyle.)
+//    }
+}
---
+++ api/src/main/java/com/hva/nl/ewa/models/TreasureStyle.java
@@ -0,0 +1,30 @@
+package com.hva.nl.ewa.models;
+
+public class TreasureStyle {
+
+    public static final String BATCARD = "/assets/images/cards/bat.png";
+    public static final String BOOKCARD = "/assets/images/cards/book.png";
+    public static final String BUGCARD = "/assets/images/cards/bug.png";
+    public static final String CANDLECARD = "/assets/images/cards/candle.png";
+    public static final String CHESTCARD = "/assets/images/cards/chest.png";
+    public static final String CROWNCARD = "/assets/images/cards/crown.png";
+    public static final String DRAGONCARD = "/assets/images/cards/dragon.png";
+    public static final String FLYTHINGCARD = "/assets/images/cards/fly-thing.png";
+    public static final String GEMCARD = "/assets/images/cards/gem.png";
+    public static final String GHOSTCARD = "/assets/images/cards/ghost.png";
+    public static final String GHOSTUGLYCARD = "/assets/images/cards/ghost-ugly.png";
+    public static final String GOLDCARD = "/assets/images/cards/gold.png";
+    public static final String HELMETCARD = "/assets/images/cards/helmet.png";
+    public static final String KEYSCARD = "/assets/images/cards/keys.png";
+    public static final String MAPCARD = "/assets/images/cards/map.png";
+    public static final String MOUSECARD = "/assets/images/cards/mouse.png";
+    public static final String OWLCARD = "/assets/images/cards/owl.png";
+    public static final String RINGCARD = "/assets/images/cards/ring.png";
+    public static final String SALAMANDERCARD = "/assets/images/cards/salamander.png";
+    public static final String SKULLCARD = "/assets/images/cards/skull.png";
+    public static final String SPIDERCARD = "/assets/images/cards/spider.png";
+    public static final String SWORDCARD = "/assets/images/cards/sword.png";
+    public static final String UGLYASSCARD = "/assets/images/cards/ugly-ass.png";
+    public static final String WITCHCARD = "/assets/images/cards/witch.png";
+
+}
