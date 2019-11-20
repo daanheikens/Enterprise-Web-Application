179e0bc39cdd94f0882ee0071c3473a785f24c6e
griends1 <Steelpan1>
2019-11-20 21:00:24 +0100

    treasure

---
+++ api/src/main/java/com/hva/nl/ewa/models/TreasureDetection.java
@@ -0,0 +1,73 @@
+package com.hva.nl.ewa.models;
+
+public class TreasureDetection {
+
+    private Tile tile;
+    private Pawn pawn;
+    private int scorePlayer = 0;
+
+    private boolean treasureCard;
+    //TODO klasse aanmaken voor treasureCard?
+
+    public TreasureDetection(Tile tile, Pawn pawn) {
+        this.tile = tile;
+        this.pawn = pawn;
+    }
+
+    private boolean checkPlayerHasTreasure() {
+        return treasureCard;
+    }
+
+    private boolean checkPlayerTile() {
+        return this.tile != null;
+    }
+
+    private boolean compareTilePawnId() {
+        //return tile.getTileId() == pawn.getTileId();
+        return true;
+    }
+
+    private boolean compareTreasureTile() {
+        //return compareTilePawnId() ==
+        //TODO treasurecard
+        return true;
+    }
+
+    private void addPointForPlayer() {
+        if (compareTreasureTile()) {
+            scorePlayer++;
+            removeTreasureCard();
+        }
+    }
+
+    private void removeTreasureCard() {
+        //TODO de huidige Treasure card verwijderen uit voor de speler
+    }
+
+    private void checkForNewCard() {
+        //TODO controleren of er een nieuwe kaart is voor de speler
+    }
+
+    //Getters and Setters
+    public int getScorePlayer() {
+        return scorePlayer;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public void setTile(Tile tile) {
+        this.tile = tile;
+    }
+
+    public Pawn getPawn() {
+        return pawn;
+    }
+
+    public void setPawn(Pawn pawn) {
+        this.pawn = pawn;
+    }
+
+
+}
