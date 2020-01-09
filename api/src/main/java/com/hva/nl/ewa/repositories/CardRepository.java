c315667dcf4fef68a77ee82f1ced82301abb400e
Lars <lars-bs@outlook.com>
2020-01-09 19:17:26 +0100

    card repository aangepast voor het ophalen van current card in game voor user.

--- api/src/main/java/com/hva/nl/ewa/repositories/CardRepository.java
+++ api/src/main/java/com/hva/nl/ewa/repositories/CardRepository.java
@@ -1,9 +1,13 @@
 package com.hva.nl.ewa.repositories;
 
 import com.hva.nl.ewa.models.Card;
+import com.hva.nl.ewa.models.Game;
+import com.hva.nl.ewa.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
+
+    Card findFirstByUserAndGame(User user, Game game);
 }
